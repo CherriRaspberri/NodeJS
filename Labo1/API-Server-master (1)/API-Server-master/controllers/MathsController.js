0d7a40e5e979adfa78c43dc9192ec91b7b587319
CherriRaspberri <103427209+CherriRaspberri@users.noreply.github.com>
2022-09-14 14:00:22 -0400

    Publish

--- Labo1/API-Server-master (1)/API-Server-master/controllers/MathsController.js
+++ Labo1/API-Server-master (1)/API-Server-master/controllers/MathsController.js
@@ -7,6 +7,33 @@ module.exports =
             this.params = HttpContext.path.params;
         }
 
+        factorial(n) {
+            if (n === 0 || n === 1) {
+                return 1;
+            }
+            return n * factorial(n - 1);
+        }
+
+        isPrime(value) {
+            for (var i = 2; i < value; i++) {
+                if (value % i === 0) {
+                    return false;
+                }
+            }
+            return value > 1;
+        }
+
+        findPrime(n) {
+            let primeNumer = 0;
+            for (let i = 0; i < n; i++) {
+                primeNumer++;
+                while (!isPrime(primeNumer)) {
+                    primeNumer++;
+                }
+            }
+            return primeNumer;
+        }
+
         checkParamCount(nbParams) {
             if (Object.keys(this.params).length > nbParams) {
                 return this.error("too many parameters");
@@ -16,8 +43,8 @@ module.exports =
 
         operate(x, y, op) {
             let result = 0;
-            switch(op) {
-                case '+': 
+            switch (op) {
+                case '+':
                     result = x + y;
                     break;
                 case '-':
@@ -33,23 +60,29 @@ module.exports =
                     result = x % y;
                     break;
                 case '!':
-                    result = x;
+                    result = this.factorial(x);
                     break;
                 case 'p':
-                    result = x;
+                    result = this.isPrime(x);
                     break;
                 case 'np':
-                    result = x;
+                    result = this.findPrime(x);
                     break;
             }
             return result;
         }
 
         get() {
-            this.HttpContext.response.JSON(params);
+
+            if (this.params != null) {
+                this.HttpContext.response.JSON(this.params);
+            }
+            else {
+                this.help();
+            }
         }
 
         help() {
-
+            this.HttpContext.response.HTML();
         }
     }
